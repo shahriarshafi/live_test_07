e8ac7d931317b19b46911d1227208fa6f3280122
Shahriar Rubiat <shahriarshafi@gmail.com>
2023-06-19 23:53:22 +0600

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,112 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:quiz_test/second_screen.dart';
 
-void main(){
-  runApp(MyApp());
+
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CounterApp(),
+    );
+  }
+}
+
+class CounterApp extends StatefulWidget {
+  const CounterApp({super.key});
+
+  @override
+  State<CounterApp> createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> {
+  int number = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Counter App"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Counter value = $number", style: const TextStyle(fontSize: 30),),
+            const SizedBox(height: 15),
+            Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.zero,
+                            backgroundColor: Colors.green
+                        ),
+                        onPressed: (){
+                          setState(() {
+                            number++;
+                            if(number == 5){
+                              showDialog(context: context, builder: (context) {
+                                return AlertDialog(
+                                  title: Text("Counter Alert"),
+                                  content: Text("Counter Value is 5!"),
+                                  actions: [
+                                    TextButton(onPressed: (){
+                                      Navigator.pop(context);
+                                    }, child: const Text("OK")
+                                    )
+                                  ],
+                                );
+                              },
+                              );
+                            }
+                            if(number == 10){
+                              Navigator.push(context,
+                                  MaterialPageRoute(builder: (context) => const SecondScreen(),));
+                            }
+                          });
+                        },
+                        child: const Text("Increment")
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 15),
+                Expanded(
+                  child: SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            padding: EdgeInsets.zero,
+                            backgroundColor: Colors.red
+                        ),
+                        onPressed: (){
+                          setState(() {
+                            number--;
+                          });
+                        },
+                        child: const Text("Decrement")
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
